ca9294f637bbb11c7674e622cb268071fae6ad32
adashuai5 <yd.zhou@foxmail.com>
2020-07-21 23:20:08 +0800

    Button Component

---
+++ components/Button/index.js
@@ -0,0 +1,24 @@
+Component({
+  properties: {
+    innerText: {
+      type: String,
+      value: "",
+    },
+    type: {
+      type: String,
+      value: "default",
+    },
+    size: {
+      type: String,
+      value: "default",
+    },
+  },
+  data: {
+    // 这里是一些组件内部数据
+    someData: {},
+  },
+  methods: {
+    // 这里是一个自定义方法
+    customMethod: function () {},
+  },
+});
